0d7597efdd81ea38b5ab21022338aa035deaf94c
kimgalina <genrietta.kim@gmail.com>
2023-10-31 19:19:40 +0600

    documentation for ActionHandler, ActionController

--- ActionHandler.java
+++ ActionHandler.java
@@ -1,5 +1,17 @@
 import java.awt.event.ActionEvent;
 
+/**
+ * The ActionHandler interface defines the contract for classes that handle specific actions triggered
+ * by the user interface, such as menu item clicks or button presses. Classes implementing this interface
+ * should provide the logic to respond to these actions by implementing the handleAction method.
+ */
 public interface ActionHandler {
+
+    /**
+     * Handles a specific action based on the provided action command and ActionEvent.
+     *
+     * @param command The action command associated with the action.
+     * @param event   The ActionEvent object representing the user's action.
+     */
     void handleAction(String command, ActionEvent event);
 }
--- Main.java
+++ Main.java
@@ -1,4 +1,15 @@
+/**
+ * The Main class serves as the entry point for the application. It contains the main method
+ * responsible for starting the application by initializing the Viewer and invoking its
+ * startApplication method.
+ */
 public class Main {
+
+    /**
+     * The main method, which serves as the entry point for the application.
+     *
+     * @param args The command-line arguments passed to the application (not used in this case).
+     */
     public static void main(String[] args) {
         Viewer viewer = new Viewer();
         viewer.startApplication();
