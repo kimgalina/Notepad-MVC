a24435462ac90759ec75803ed45634cfa95c60ed
kimgalina <genrietta.kim@gmail.com>
2023-10-31 22:16:00 +0600

    completely finished documentation

--- TabsController.java
+++ TabsController.java
@@ -5,6 +5,9 @@ import java.util.List;
 import java.io.File;
 import javax.swing.JButton;
 
+/**
+ * TabsController manages unsaved changes and associated files for each tab in the Viewer.
+ */
 public class TabsController implements DocumentListener {
     private List<Boolean> unsavedChangesPerTab;
     private List<File> filesPerTabs;
@@ -12,6 +15,11 @@ public class TabsController implements DocumentListener {
     private JButton closeBtn;
     private boolean isFileOpening;
 
+    /**
+    * Constructs a TabsController for the given Viewer.
+    *
+    * @param viewer The Viewer instance this controller is associated with.
+    */
     public TabsController(Viewer viewer) {
         unsavedChangesPerTab = new ArrayList<>();
         filesPerTabs = new ArrayList<>();
@@ -19,18 +27,39 @@ public class TabsController implements DocumentListener {
         isFileOpening = false;
     }
 
+
+    /**
+     * Gets the list of unsaved changes per tab.
+     *
+     * @return A list containing Boolean values indicating unsaved changes for each tab.
+     */
     public List<Boolean> getUnsavedChangesPerTab() {
         return unsavedChangesPerTab;
     }
 
+    /**
+    * Gets the list of associated files per tab.
+    *
+    * @return A list of File objects representing the files associated with each tab.
+    */
     public List<File> getFilesPerTabs() {
         return filesPerTabs;
     }
 
+    /**
+    * Sets the flag indicating whether a file is being opened in the Viewer.
+    *
+    * @param value True if a file is being opened, false otherwise.
+    */
     public void setIsFileOpening(boolean value) {
         isFileOpening = value;
     }
 
+   /**
+    * Handles an insert update in the document for the current tab.
+    *
+    * @param e The DocumentEvent representing the insert update.
+    */
     @Override
     public void insertUpdate(DocumentEvent e) {
         int currentTabIndex = viewer.getCurrentTabIndex();
@@ -40,6 +69,11 @@ public class TabsController implements DocumentListener {
         }
     }
 
+   /**
+    * Handles a remove update in the document for the current tab.
+    *
+    * @param e The DocumentEvent representing the remove update.
+    */
     @Override
     public void removeUpdate(DocumentEvent e) {
         int currentTabIndex = viewer.getCurrentTabIndex();
@@ -58,11 +92,26 @@ public class TabsController implements DocumentListener {
         }
     }
 
+   /**
+    * Handles a changed update in the document for the current tab.
+    *
+    * @param e The DocumentEvent representing the changed update.
+    */
     @Override
     public void changedUpdate(DocumentEvent e) {
         System.out.println("changed content");
     }
 
+
+    /**
+     * Fills a list with a specific value up to the specified index.
+     *
+     * @param list  The list to be filled.
+     * @param size  The desired size of the list.
+     * @param index The index up to which the list should be filled.
+     * @param value The value to fill in the list.
+     * @param <T>   The type of elements in the list.
+     */
     public <T> void fillList(List<T> list, int size, int index, T value) {
         if(index == list.size()) {
             list.add(index, value);
@@ -76,6 +125,14 @@ public class TabsController implements DocumentListener {
         }
     }
 
+    /**
+     * Sets a specific value in the list at the given index, expanding the list if necessary.
+     *
+     * @param list           The list in which to set the value.
+     * @param currentTabIndex The index at which to set the value.
+     * @param value          The value to set.
+     * @param <T>            The type of elements in the list.
+     */
     public <T> void setValueInToList(List<T> list, int currentTabIndex, T value) {
         if(currentTabIndex < list.size()) {
             list.set(currentTabIndex, value);
